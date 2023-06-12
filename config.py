f7d40d5ee9864869792392d55e118ef6574cf95f
NikitaSasim <101585298+NikitaSasim@users.noreply.github.com>
2023-06-12 16:00:58 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-
-BOT_TOKEN = "6289859327:AAGOCeb5s7413vrlJx6qIhAQE5QnMfQIlts"
-OPENAI_TOKEN = "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"
-ACC_TOKEN = "fdsf57hl8sl8H7cH&huhcsd7re8hH&kksdch&H473fc8HH74hf"
-URL = "https://nikacc.herokuapp.com"
